fddf0183888c75721d9b50d6a50dc2054c716d2e
Hüseyin Şimşek <Huseyin.Simsek@obase.com>
2024-02-23 14:37:37 +0300

    Add project files.

---
+++ HelloMVC/Controllers/CetController.cs
@@ -0,0 +1,33 @@
+﻿using HelloMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace HelloMVC.Controllers
+{
+    public class CetController : Controller
+    {
+        public IActionResult test()
+        {
+            return View();
+        }
+        public IActionResult hello(string name, string surname)
+        {
+            Student student = new Student(); ;
+            student.Name = name;
+            student.Surname = surname;
+
+            return View(student);
+           // return Content($"<html><head><title>Merhaba</title></head><body><h1>Merhaba {name} {surname}</h1></body></html>");
+            //return "Merhaba " + name + " "+ surname;
+
+        }
+
+        public IActionResult helloStudent(Student student)
+        {
+            return View(student);
+            // return Content($"<html><head><title>Merhaba</title></head><body><h1>Merhaba {name} {surname}</h1></body></html>");
+            //return "Merhaba " + name + " "+ surname;
+
+        }
+    }
+}
